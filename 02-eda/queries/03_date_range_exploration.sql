eb6bf1838278358e8dce1508b287e4c5d0951d7f
Cristian Cervantes <crisn2blink@gmail.com>
2026-06-26 08:00:23 -0500

    Add queries for customer age and new customers per year

--- 02-eda/queries/03_date_range_exploration.sql
+++ 02-eda/queries/03_date_range_exploration.sql
@@ -18,10 +18,25 @@ SELECT
     DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
 FROM gold.fact_sales;
 
--- Find the youngest and oldest customer based on birthdate
+-- Find the youngest age and oldest age of customers based on birthdate
 SELECT
     MIN(birthdate) AS oldest_birthdate,
     DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
     MAX(birthdate) AS youngest_birthdate,
     DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
 FROM gold.dim_customers;
+
+--Find the number of new customers per year
+SELECT
+    first_order_year,
+    COUNT(*) AS first_time_customers
+FROM
+    (
+    SELECT
+        customer_key,
+        YEAR(MIN(order_date)) AS first_order_year
+    FROM gold.fact_sales
+    GROUP BY customer_key
+    ) AS t
+GROUP BY first_order_year
+ORDER BY first_order_year
